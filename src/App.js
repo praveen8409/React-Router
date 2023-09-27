7b2033138262e7f1abc77390f16da3a58edf5fbb
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-09-27 19:58:37 +0530

    We will go through createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Item from "./pages/Items"
 function App() {
-  const routs = createRoutesFromElements(
-    <>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/item" element={<Item />} />
-    </>
-  );
+  // const routs = createRoutesFromElements(
+  //   <>
+  //   <Route path="/" element={<Home />} />
+  //   <Route path="/about" element={<About />} />
+  //   <Route path="/item" element={<Item />} />
+  //   </>
+  // );
 
-  const router = createBrowserRouter(routs);
+  // const router = createBrowserRouter(routs);
 
-  // const router = createBrowserRouter([
-  //   { path: "/", element: <Home /> },
-  //   { path: "/about", element: <About /> },
-  //   { path: "/items", element: <Item /> },
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/about", element: <About /> },
+    { path: "/items", element: <Item /> },
 
-  // ]);
+  ]);
 
   return <>
     <RouterProvider router={router} />
