6ebecb126fb5381f1b4a4ff1c68bd690523113fc
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-09-27 19:42:48 +0530

    Create route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Item from "./pages/Items"
 function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/about", element: <About /> },
+    { path: "/items", element: <Item /> },
 
+  ]);
   
   return <>
+   <RouterProvider router={router} />
   </>;
 }
 
